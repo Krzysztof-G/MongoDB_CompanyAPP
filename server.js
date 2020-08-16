3c40c4e56bda64c1f6b427ac1ae04adb938f9194
Krzysztof Gumuła <k.gumula@icloud.com>
2020-08-16 09:09:39 +0200

    change endpoints on routes files

--- server.js
+++ server.js
@@ -18,6 +18,11 @@ mongoClient.connect('mongodb://localhost:27017/', { useNewUrlParser: true, useUn
     app.use(cors());
     app.use(express.json());
     app.use(express.urlencoded({ extended: false }));
+
+    app.use((req, res, next) => {
+      req.db = db;
+      next();
+    });
     
     app.use('/api', employeesRoutes);
     app.use('/api', departmentsRoutes);
